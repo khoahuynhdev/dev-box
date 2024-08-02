fae0d08785dd7215f43bdec38327712892f84581
KhoaHuynhdev <khoa0319@gmail.com>
2024-08-02 16:25:53 +0700

    feat: install some default software

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env bash
 
-
 set -euxo pipefail
 
 sudo apt-get update -y
-apt-get install -y software-properties-common curl apt-transport-https ca-certificates vim
+apt-get install -y software-properties-common curl apt-transport-https ca-certificates build-essential
+
+# install neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt-get update -y && sudo apt-get install -y neovim
